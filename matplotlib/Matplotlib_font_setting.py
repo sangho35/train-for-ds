9bc1bfdfb89bd7eed8655a642269a95c8b92688f
sangho35 <42054177+sangho35@users.noreply.github.com>
2021-01-22 12:03:29 +0900

    Add files via upload

---
+++ matplotlib/Matplotlib_font_setting.py
@@ -0,0 +1,15 @@
+import matplotlib
+from matplotlib import font_manager, rc
+import platform
+
+try : 
+    if platform.system() == 'Windows':
+    # 윈도우인 경우
+        font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+        rc('font', family=font_name)
+    else:    
+    # Mac 인 경우
+        rc('font', family='AppleGothic')
+except : 
+    pass
+matplotlib.rcParams['axes.unicode_minus'] = False   
